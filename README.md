2a6b07790e21bcb74d7b066a866006a0dbbc3dc3
rudraksh73 <78246276+rudraksh73@users.noreply.github.com>
2021-02-06 17:37:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# collision-algorithm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch-1.js
@@ -0,0 +1,30 @@
+var fixedRect, movingRect
+ function setup() { 
+   createCanvas(800,400);
+    fixedRect = createSprite(200, 200, 50, 80);
+     fixedRect.shapeColor = "green"
+      movingRect = createSprite(400, 200, 80, 30); 
+      movingRect.shapeColor = "green" 
+      fixedRect.debug = true;
+       movingRect.debug = true;
+       } 
+       function draw() {
+          background(0,0,0);
+           console.log(movingRect.x-fixedRect.x) 
+           movingRect.x = World.mouseX 
+           movingRect.y = World.mouseY 
+ 
+           if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 &&
+             fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 &&
+              movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 && 
+              fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+                 movingRect.shapeColor = "blue"; 
+                 fixedRect.shapeColor = "blue"; 
+                }
+                 else {
+                    movingRect.shapeColor = "green";
+                     fixedRect.shapeColor = "green"; 
+                    }
+
+                    drawSprites();
+                  }
